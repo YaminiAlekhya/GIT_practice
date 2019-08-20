33ca1be9ec9631345e0481503ee743d0e4495ddd
yaminialekhya <alekhya@gmail.com>
2019-08-20 19:05:49 +0530

    commit all changes at once

---
+++ addnumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class addnumbers
+{
+	public statis void main(String args[])
+	{
+		int x=10;
+		int y=20;
+		int z;
+		z=x+y;
+		System.out.println("sum of x and y is " +z);
+
+	}
+}
+
--- helloworld.java
+++ helloworld.java
@@ -5,6 +5,7 @@ class helloworld
 		System.out.println("hello world");
 		System.out.println("hello world");
 
+		System.out.println("hello world");
 
 	}
 }
